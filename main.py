8da253a53fc903bf301926261bea1d7e107826ce
mr_maroga <45645052+AdvocateM@users.noreply.github.com>
2022-05-31 02:30:40 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,13 @@
 
 import subprocess
 
-subprocess.call('ifconfig wlan0 down', shell=True)
-subprocess.call('ifconfig wlan0 hw ether 40:49:0f:80:3c:5b', shell=True)
+interface = "wlan0"
+mac_id = '40:49:0f:80:3c:7b'
+new_mac = '40:49:0f:80:3c:5b'
+current_id = "ifconfig wlan0 | grep ether"
+
+print(f'[+] Changing MAC Address for {interface}  from {mac_id} to new mac')
+
+subprocess.call(f'ifconfig {interface} down', shell=True)
+subprocess.call(f'ifconfig wlan0 hw ether {new_mac}', shell=True)
 subprocess.call('ifconfig wlan0 up', shell=True)
